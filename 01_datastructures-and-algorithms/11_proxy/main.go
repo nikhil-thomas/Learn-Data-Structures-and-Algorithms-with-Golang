b5566782c15faa9825c38d4730aab2d1eab0bb70
Nikhil Thomas <nikhilthomas1@gmail.com>
2019-07-31 07:49:47 +0530

    Add proxy pattern

--- 01_datastructures-and-algorithms/11_proxy/main.go
+++ 01_datastructures-and-algorithms/11_proxy/main.go
@@ -1,40 +1,29 @@
-	//main package has examples shown
-// in Go Data Structures and algorithms book
 package main
 
-// importing fmt package
-import (
-	"fmt"
-)
+import "fmt"
 
-//IRealObject interface
 type IRealObject interface {
 	performAction()
 }
 
-//RealObject struct
-type RealObject struct{}
+type RealObject struct {}
 
-//RealObject class method performAction
 func (realObject *RealObject) performAction() {
-	fmt.Println("RealObject performAction()")
+	fmt.Println("RealObject perform action")
 }
 
-//VirtualProxy struct
 type VirtualProxy struct {
 	realObject *RealObject
 }
 
-//VirtualProxy class method performAction
 func (virtualProxy *VirtualProxy) performAction() {
 	if virtualProxy.realObject == nil {
 		virtualProxy.realObject = &RealObject{}
 	}
-	fmt.Println("Virtual Proxy performAction()")
+	fmt.Println("Virtual proxy perform action")
 	virtualProxy.realObject.performAction()
 }
 
-// main method
 func main() {
 	var object VirtualProxy = VirtualProxy{}
 	object.performAction()
